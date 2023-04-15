6c1e89fcfe4d56dc44d7a7b6e6217dc068d1b5b0
NickLangat <nickson@reveall.co>
2023-04-15 11:13:24 +0300

    first commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,92 @@
+<template>
+    <aside id="logo-sidebar" class="fixed top-0 bg-[#121212] left-0 z-40 w-96 h-screen pt-3 transition-transform -translate-x-full shadow-lg border-r border-[#121212] sm:translate-x-0" aria-label="Sidebar">
+       <div class="h-full px-3 pb-4 overflow-y-auto bg-theme">
+          <div class="flex gap-2">
+             <svg viewBox="0 0 16 16" class="h-10 w-10 text-white" xmlns="http://www.w3.org/2000/svg">
+                <path
+                   d="M8 0a8 8 0 100 16A8 8 0 008 0zm3.903 11.532a.695.695 0 01-.947.267 8.299 8.299 0 00-3.237-1.02 8.318 8.318 0 00-3.384.351.696.696 0 01-.423-1.326 9.668 9.668 0 013.948-.409 9.703 9.703 0 013.777 1.19.697.697 0 01.266.947zm.958-2.429a.695.695 0 01-.938.295 10.87 10.87 0 00-3.944-1.177 10.828 10.828 0 00-4.127.367.695.695 0 11-.386-1.336 12.264 12.264 0 014.653-.415c1.566.159 3.062.606 4.447 1.327.34.178.473.598.295.939zm.346-2.022a.69.69 0 01-.31-.073 13.454 13.454 0 00-4.659-1.345 13.423 13.423 0 00-4.843.383.695.695 0 11-.359-1.344 14.85 14.85 0 015.343-.423c1.802.183 3.532.682 5.139 1.484a.695.695 0 01-.311 1.318z"
+                   fill="currentColor"/>
+             </svg>
+             <h3 class=" font-Pacifico text-[26px] text-white font-bold mb-8">
+                Spotify
+             </h3>
+          </div>
+          <ul class="space-y-4 font-medium">
+             <li>
+                <a href="/" class="flex items-center p-2 text-white text-sm">
+                   <svg viewBox="0 0 512 512" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                      <path
+                         d="M 256.274 60.84 L 84.324 166.237 L 84.324 443.063 L 193.27 443.063 L 193.27 293.73 L 320.228 293.73 L 320.228 443.063 L 428.222 443.063 L 428.222 165.476 L 256.274 60.84 Z M 256.274 35.95 L 448.452 149.145 L 448.452 464.395 L 300 464.395 L 300 315.062 L 213.499 315.062 L 213.499 464.395 L 64.095 464.395 L 64.095 150.161 L 256.274 35.95 Z"
+                         fill="currentColor"/>
+                   </svg>
+                   <span class="ml-3">Home</span>
+                </a>
+             </li>
+             <li>
+                <a href="/" class="flex items-center p-2 text-gray-400 hover:text-white text-sm">
+                   <svg viewBox="0 0 512 512" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                      <path
+                         d="M349.714 347.937l93.714 109.969-16.254 13.969-93.969-109.969q-48.508 36.825-109.207 36.825-36.826 0-70.476-14.349t-57.905-38.603-38.603-57.905-14.349-70.476 14.349-70.476 38.603-57.905 57.905-38.603 70.476-14.349 70.476 14.349 57.905 38.603 38.603 57.905 14.349 70.476q0 37.841-14.73 71.619t-40.889 58.921zM224 377.397q43.428 0 80.254-21.461t58.286-58.286 21.461-80.254-21.461-80.254-58.286-58.285-80.254-21.46-80.254 21.46-58.285 58.285-21.46 80.254 21.46 80.254 58.285 58.286 80.254 21.461z"
+                         fill="currentColor" fillRule="evenodd"/>
+                   </svg>
+                   <span class="ml-3">Search</span>
+                </a>
+             </li>
+             <li>
+                <a href="/" class="flex items-center p-2 text-gray-400 hover:text-white text-sm">
+                   <svg viewBox="0 0 512 512" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                      <path
+                         d="M291.301 81.778l166.349 373.587-19.301 8.635-166.349-373.587zM64 463.746v-384h21.334v384h-21.334zM192 463.746v-384h21.334v384h-21.334z"
+                         fill="currentColor"/>
+                   </svg>
+                   <span class="ml-3">Your Library</span>
+                </a>
+             </li>
+             <li class="pb-4">
+             </li>
+             <li>
+                <a href="/" class="flex items-center p-2 text-gray-400 hover:text-white text-sm">
+                   <svg viewBox="0 0 16 16" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                      <path d="M14 7H9V2H7v5H2v2h5v5h2V9h5z" fill="currentColor"/>
+                      <path fill="none" d="M0 0h16v16H0z"/>
+                   </svg>
+                   <span class="ml-3">Create Playlist</span>
+                </a>
+             </li>
+             <li>
+                <a href="/" class="flex items-center p-2 text-gray-400 hover:text-white text-sm">
+                   <svg viewBox="0 0 16 16" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                      <path fill="none" d="M0 0h16v16H0z"/>
+                      <path fill="currentColor"
+                         d="M13.797 2.727a4.057 4.057 0 00-5.488-.253.558.558 0 01-.31.112.531.531 0 01-.311-.112 4.054 4.054 0 00-5.487.253c-.77.77-1.194 1.794-1.194 2.883s.424 2.113 1.168 2.855l4.462 5.223a1.791 1.791 0 002.726 0l4.435-5.195a4.052 4.052 0 001.195-2.883 4.057 4.057 0 00-1.196-2.883z"/>
+                   </svg>
+                   <span class="ml-3">Liked Songs</span>
+                </a>
+             </li>
+             <hr class="h-px my-8 bg-gray-700 border-0">
+             <li> <span class="ml-3 text-gray-400 hover:text-white text-sm">2023</span></li>
+          </ul>
+          <div class="mt-72">
+             <div class="mt-64 text-gray-400 hover:text-white cursor-pointer gap-4 font-semibold flex justify-start">
+                <svg viewBox="0 0 24 24" :width=size :height=size xmlns="http://www.w3.org/2000/svg">
+                   <path fill="currentColor"
+                      d="M11.5 0C5.149 0 0 5.148 0 11.5 0 17.851 5.149 23 11.5 23S23 17.851 23 11.5C23 5.148 17.851 0 11.5 0zm0 22C5.71 22 1 17.29 1 11.5S5.71 1 11.5 1 22 5.71 22 11.5 17.29 22 11.5 22zm.499-6.842V5h-1v10.149l-3.418-3.975-.758.652 4.678 5.44 4.694-5.439-.757-.653-3.439 3.984z"/>
+                </svg>
+                Install App
+             </div>
+          </div>
+       </div>
+    </aside>
+</template>
+
+<script>
+export default {
+        data() {
+            return {
+                isOpen: false,
+                size: 22,
+            }
+        }
+}
+    
+ </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,218 @@
+<template>
+  <div>
+     <Sidebar />
+     <div class="p-4 sm:ml-96">
+        <div>
+           <div class="flex justify-between">
+              <div class="flex text-white gap-4">
+                 <span class="rounded-full bg-[#121212] px-1 py-1 cursor-pointer">
+                    <svg viewBox="0 0 24 24" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <polyline stroke="currentColor" points="16 4 7 12 16 20" fill="none"/>
+                    </svg>
+                 </span>
+                 <span class="rounded-full bg-[#121212] px-1 py-1 cursor-pointer">
+                    <svg viewBox="0 0 24 24" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <polyline stroke="currentColor" points="8 4 17 12 8 20" fill="none"/>
+                    </svg>
+                 </span>
+              </div>
+              <button class="bg-[#121212] text-white rounded-full px-4 py-0.5">
+                 <div class="flex justify-between gap-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 mt-0.5">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                    </svg>
+                    <span>f9trssfshjnbvv543</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                       <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                 </div>
+              </button>
+           </div>
+           <div class="mt-8">
+              <h3 class="text-white font-bold text-4xl">Good evening</h3>
+           </div>
+           <div class="mt-8 grid grid-cols-3 gap-x-8 gap-y-6">
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/davido.webp" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Davido</h3>
+                 </div>
+              </div>
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/Mavado.webp" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Mavado</h3>
+                 </div>
+              </div>
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/magix.webp" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Magixx</h3>
+                 </div>
+              </div>
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/Chris_Brown_5,_2012.jpeg" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Chris Brown</h3>
+                 </div>
+              </div>
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/burna.png" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Burna Boy</h3>
+                 </div>
+              </div>
+              <div class="flex bg-[#121212] hover:bg-[#363737] rounded">
+                 <div class="w-1/3">
+                    <img class="object-cover rounded-l h-20 w-20" src="../assets/mimi.jpeg" alt="">
+                 </div>
+                 <div class="w-2/3 mt-6">
+                    <h3 class="text-white text-xl font-bold">Mimi Mars</h3>
+                 </div>
+              </div>
+           </div>
+           <div class="mt-8">
+              <h3 class="text-white font-semibold text-2xl">Jump back in</h3>
+           </div>
+           <div class="grid grid-cols-4 gap-x-6 mt-6">
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded px-4" src="../assets/animal.webp" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Animal Instinct Riddim</h3>
+                    <p class="text-gray-400 text-sm">Various Artists</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/ruby.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Ruby</h3>
+                    <p class="text-gray-400 text-sm">Artist</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/YKB-Picture.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">YKB</h3>
+                    <p class="text-gray-400 text-sm">Artist</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/wande_coal.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Wande Coal</h3>
+                    <p class="text-gray-400 text-sm">Artist</p>
+                 </div>
+              </div>
+           </div>
+           <div class="mt-8">
+              <h3 class="text-white font-semibold text-2xl">Made for you</h3>
+           </div>
+           <div class="grid grid-cols-4 gap-x-6 mt-6 pb-32">
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/Iyanya-One-Side-Artwork.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">One side</h3>
+                    <p class="text-gray-400 text-sm">Song</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/love.webp" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Love don't cost...</h3>
+                    <p class="text-gray-400 text-sm">Song</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/Omah-Lay-Soso-1000x1000.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Soso</h3>
+                    <p class="text-gray-400 text-sm">Song</p>
+                 </div>
+              </div>
+              <div class="hover:bg-[#121212] bg-[#292929] rounded-lg">
+                 <img class="pt-4 rounded-lg px-4" src="../assets/Victony-profile.jpeg" alt="">
+                 <div class="ml-6 mt-2 pb-4">
+                    <h3 class="text-white font-semibold">Victony</h3>
+                    <p class="text-gray-400 text-sm">Artist</p>
+                 </div>
+              </div>
+           </div>
+           <footer class="fixed bottom-0 left-96 z-20 w-full p-4 hover:bg-[#121212] bg-[#292929] border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6">
+              <div class="flex justify-between">
+                 <div class="flex gap-2 text-white">
+                    <img class="h-10 w-10" src="../assets/loyal.jpeg" alt="">
+                    <div class="flex mb-2 flex-col">
+                       <p>Loyal</p>
+                       <p class="text-xs">Magixx</p>
+                    </div>
+                 </div>
+                 <div class="flex text-white font-bold gap-6 ml-72 items-center justify-center">
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path
+                          d="M4.5 6.8l.7-.8C4.1 4.7 2.5 4 .9 4v1c1.3 0 2.6.6 3.5 1.6l.1.2zm7.5 4.7c-1.2 0-2.3-.5-3.2-1.3l-.6.8c1 1 2.4 1.5 3.8 1.5V14l3.5-2-3.5-2v1.5zm0-6V7l3.5-2L12 3v1.5c-1.6 0-3.2.7-4.2 2l-3.4 3.9c-.9 1-2.2 1.6-3.5 1.6v1c1.6 0 3.2-.7 4.2-2l3.4-3.9c.9-1 2.2-1.6 3.5-1.6z"
+                          fill="currentColor"/>
+                    </svg>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path d="M13 2.5L5 7.119V3H3v10h2V8.881l8 4.619z" fill="currentColor"/>
+                    </svg>
+                    <span class="bg-white text-black rounded-full px-1 py-1">
+                       <svg viewBox="0 0 24 24" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                          <polygon points="21.57 12 5.98 3 5.98 21 21.57 12" fill="currentColor"/>
+                       </svg>
+                    </span>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path d="M11 3v4.119L3 2.5v11l8-4.619V13h2V3z" fill="currentColor"/>
+                    </svg>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path
+                          d="M5.5 5H10v1.5l3.5-2-3.5-2V4H5.5C3 4 1 6 1 8.5c0 .6.1 1.2.4 1.8l.9-.5C2.1 9.4 2 9 2 8.5 2 6.6 3.6 5 5.5 5zm9.1 1.7l-.9.5c.2.4.3.8.3 1.3 0 1.9-1.6 3.5-3.5 3.5H6v-1.5l-3.5 2 3.5 2V13h4.5C13 13 15 11 15 8.5c0-.6-.1-1.2-.4-1.8z"
+                          fill="currentColor"/>
+                    </svg>
+                 </div>
+                 <div class="flex text-white font-bold gap-4 ml-72 items-center justify-center">
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path
+                          d="M8.5 1A4.505 4.505 0 004 5.5c0 .731.191 1.411.502 2.022L1.99 13.163a1.307 1.307 0 00.541 1.666l.605.349a1.307 1.307 0 001.649-.283L9.009 9.95C11.248 9.692 13 7.807 13 5.5 13 3.019 10.981 1 8.5 1zM4.023 14.245a.307.307 0 01-.388.066l-.605-.349a.309.309 0 01-.128-.393l2.26-5.078A4.476 4.476 0 007.715 9.92l-3.692 4.325zM8.5 9C6.57 9 5 7.43 5 5.5S6.57 2 8.5 2 12 3.57 12 5.5 10.429 9 8.5 9z"
+                          fill="currentColor"/>
+                    </svg>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path d="M2 2v5l4.33-2.5L2 2zm0 12h14v-1H2v1zm0-4h14V9H2v1zm7-5v1h7V5H9z" fill="currentColor"/>
+                    </svg>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path
+                          d="M0 3v8c0 .55.45 1 1 1h5v-1H1V3h5V2H1c-.55 0-1 .45-1 1zm3 11.5c0 .275.225.5.5.5H6v-1H3.5c-.275 0-.5.225-.5.5zM15 2H9c-.55 0-1 .45-1 1v11c0 .55.45 1 1 1h6c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm0 12H9V3h6v11zm-3-8a.75.75 0 100-1.5.75.75 0 000 1.5zm0 6a2 2 0 100-4 2 2 0 000 4zm0-3c.551 0 1 .449 1 1s-.449 1-1 1-1-.449-1-1 .449-1 1-1z"
+                          fill="currentColor"/>
+                    </svg>
+                    <svg viewBox="0 0 16 16" width=22 height=22 xmlns="http://www.w3.org/2000/svg">
+                       <path
+                          d="M0 11.032v-6h2.802l5.198-3v12l-5.198-3H0zm7 1.27v-8.54l-3.929 2.27H1v4h2.071L7 12.302zm4.464-2.314q.401-.925.401-1.956 0-1.032-.4-1.957-.402-.924-1.124-1.623L11 3.69q.873.834 1.369 1.957.496 1.123.496 2.385 0 1.262-.496 2.385-.496 1.123-1.369 1.956l-.659-.762q.722-.698 1.123-1.623z"
+                          fill="currentColor"/>
+                    </svg>
+                 </div>
+              </div>
+           </footer>
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+   Sidebar
+  }
+}
+</script>
